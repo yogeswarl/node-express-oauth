bea2c3be16a47fbb2ac36a047020cccea2320652
Soham Kamani <sohamkamani@gmail.com>
2020-05-29 00:25:07 +0530

    m1 tests are passing

--- protected-resource.js
+++ protected-resource.js
@@ -29,38 +29,6 @@ app.use(timeout)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/user-info", (req, res) => {
-	if (!req.headers.authorization) {
-		res.status(401).send("Error: client unauthorized")
-		return
-	}
-
-	const authToken = req.headers.authorization.slice("bearer ".length)
-	let userInfo = null
-	try {
-		userInfo = jwt.verify(authToken, config.publicKey, {
-			algorithms: ["RS256"],
-		})
-	} catch (e) {
-		res.status(401).send("Error: client unauthorized")
-		return
-	}
-	if (!userInfo) {
-		res.status(401).send("Error: client unauthorized")
-		return
-	}
-
-	const user = users[userInfo.userName]
-	const userWithRestrictedFields = {}
-	const scope = userInfo.scope.split(" ")
-	for (let i = 0; i < scope.length; i++) {
-		const field = scope[i].slice("permission:".length)
-		userWithRestrictedFields[field] = user[field]
-	}
-
-	res.json(userWithRestrictedFields)
-})
-
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
 	var port = server.address().port
